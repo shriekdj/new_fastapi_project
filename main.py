cbcb0b15d5189defc2411efc5e0bea99a1230873
Shrikant Dhayje <35664193+shriekdj@users.noreply.github.com>
2022-04-18 10:26:19 +0000

    post model added

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Optional
+from pydantic import BaseModel
+
+class Post(BaseModel):
+  title: str
+  content: str
+  published: bool = True
+  rating: Optional[int] = None
+
+posts = ["post1", "post2", "post3"]
 
 app = FastAPI()
 
 @app.get("/")
 def get_root():
- return {"message": "Hello World"} 
+ return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+  return {"posts": posts}
+
+
